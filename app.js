24fe7009fc7d610120e7265181a9ce727ec29d86
hambrospecial <hamid.ibrahim@decagon.dev>
2022-01-29 02:44:20 +0100

    modify dice roll

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const btn_roll = document.querySelector('.btn_roll');
 const btn_hold = document.querySelector('.btn_hold');
-const img_el = document.querySelector('.img');
+const dice_img = document.querySelector('.img');
+const dice_container = document.querySelector('.dice');
 
 btn_roll.addEventListener('click', function(){
-    
+    dice_img.classList.add("dice_rolling");
+    dice_container.style.left = '390px';
     setTimeout(function(){
+        dice_img.classList.remove("dice_rolling");
         let diceRoll = Math.floor((Math.random() * 6) + 1);
-        img_el.src = `http://127.0.0.1:5500/dices/dice-${diceRoll}.png`
-    },500)
+        dice_container.style.left = '330px';
+        dice_img.src = `dices/dice-${diceRoll}.png`
+    },1000)
 })
+
+
 
